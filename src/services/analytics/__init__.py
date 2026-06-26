fe529732f2cc4dffcabd74159d1a508ccf3cf1de
KRYZENSYS <f91186645@gmail.com>
2026-06-26 08:31:44 +0500

    analytics: package init

---
+++ src/services/analytics/__init__.py
@@ -0,0 +1,9 @@
+from .analytics import AnalyticsService, analytics_service
+from .referral import ReferralService, referral_service
+
+__all__ = [
+    "AnalyticsService",
+    "analytics_service",
+    "ReferralService",
+    "referral_service",
+]
