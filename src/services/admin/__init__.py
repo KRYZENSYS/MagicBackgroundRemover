15299f61430d97952c01bd2009fcd8bad65e0bd0
KRYZENSYS <f91186645@gmail.com>
2026-06-26 09:11:13 +0500

    services.admin: init

---
+++ src/services/admin/__init__.py
@@ -0,0 +1,5 @@
+from .admin import AdminService
+from .broadcast import BroadcastService
+from .maintenance import maintenance_service
+
+__all__ = ["AdminService", "BroadcastService", "maintenance_service"]
