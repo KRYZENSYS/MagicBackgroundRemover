40e781700f2bb02ab21a9ff9d068f8429c0cbe66
KRYZENSYS <f91186645@gmail.com>
2026-06-26 09:08:48 +0500

    services.payment: init

--- src/services/payment/__init__.py
+++ src/services/payment/__init__.py
@@ -1,16 +1,4 @@
-from .provider import PaymentProvider, PaymentResult, PaymentStatus
-from .telegram_stars import TelegramStarsProvider
-from .click_provider import ClickProvider
-from .payme_provider import PaymeProvider
-from .manager import PaymentManager, payment_manager
+from .manager import payment_manager, PaymentManager, PaymentResult
+from .base import PaymentProvider
 
-__all__ = [
-    "PaymentProvider",
-    "PaymentResult",
-    "PaymentStatus",
-    "TelegramStarsProvider",
-    "ClickProvider",
-    "PaymeProvider",
-    "PaymentManager",
-    "payment_manager",
-]
+__all__ = ["payment_manager", "PaymentManager", "PaymentResult", "PaymentProvider"]
