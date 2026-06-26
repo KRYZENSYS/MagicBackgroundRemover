6be07d263c622aec9323740414f0d3c49ce4b820
KRYZENSYS <f91186645@gmail.com>
2026-06-26 09:09:54 +0500

    AI services: package init

--- src/services/ai/__init__.py
+++ src/services/ai/__init__.py
@@ -4,6 +4,7 @@ from .enhancer import image_enhancer
 from .effects import effects_service
 from .face import face_enhancer
 from .validator import image_validator
+from .model_manager import model_manager
 
 __all__ = [
     "bg_remover",
@@ -12,4 +13,5 @@ __all__ = [
     "effects_service",
     "face_enhancer",
     "image_validator",
+    "model_manager",
 ]
